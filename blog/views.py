46464d9895888c79d0ccb4852c265d3f817a7b3b
Boubacar Sow <boubasow.pro@gmail.com>
2022-05-12 16:40:30 -0400

    V2.0

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Boubacar',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 10 2019'
-    },
-    {
-        'author': 'Oumar',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 14 2019'
-    }
-]
-
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
